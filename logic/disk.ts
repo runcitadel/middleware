366ef4900d3680764f115ab5499d9554b996e0e3
Aaron Dewes <aaron.dewes@web.de>
2021-09-12 11:38:55 +0100

    Update dependencies

--- logic/disk.ts
+++ logic/disk.ts
@@ -1,6 +1,6 @@
 import constants from "../utils/const.js";
-import {fs_utils as diskService} from "@runcitadel/utils";
+import {fs} from "@runcitadel/utils";
 
 export function readJWTPublicKeyFile(): Promise<string> {
-  return diskService.readUtf8File(constants.JWT_PUBLIC_KEY_FILE);
+  return fs.readUtf8File(constants.JWT_PUBLIC_KEY_FILE);
 }
