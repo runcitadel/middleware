dfae34ffa247dd87c884aa1b3077b4d3faf7f56e
Philipp Walter <philippwalter@pm.me>
2022-04-25 15:04:45 +0200

    chore: rebase onto main, fix lint errors

--- ping-manager.mjs
+++ ping-manager.mjs
@@ -1,3 +1,4 @@
-const resp = await fetch(`http://${process.env.HOST}:3006/ping`);
-if (resp.status === 200) process.exit(0);
+// eslint-disable-next-line no-undef
+const response = await fetch(`http://${process.env.HOST}:3006/ping`);
+if (response.status === 200) process.exit(0);
 else process.exit(1);
