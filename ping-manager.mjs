22aff1ac7945ab67c211550210b1028fe8280558
Aaron Dewes <aaron.dewes@protonmail.com>
2022-08-30 21:40:40 +0200

    Assume manager is on port 3000

--- ping-manager.mjs
+++ ping-manager.mjs
@@ -1,3 +1,3 @@
-const response = await fetch(`http://${process.env.HOST}:3006/ping`);
+const response = await fetch(`http://${process.env.HOST}:3000/ping`);
 if (response.status === 200) process.exit(0);
 else process.exit(1);
